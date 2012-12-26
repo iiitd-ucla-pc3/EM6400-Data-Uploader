d9b1fb0a2824de3efe5a86ea69337275f70eab2b
nipunreddevil <nipunreddevil@gmail.com>
2012-12-26 20:17:44 +0530

    Added installer script

---
+++ EM6400/install.sh
@@ -0,0 +1,11 @@
+#Dependencies
+apt-get install python
+apt-get install python-pip
+apt-get install python-setuptools
+pip install pyserial
+pip install -U minimalmodbus
+apt-get install python-dev
+apt-get install libevent-dev
+pip install pyzmq gevent gevent_zeromq paste 
+pip install pytz
+
